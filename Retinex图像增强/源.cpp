825e9beda7162786ac4b46c8cc30800b5bfc63bd
WormOfSpirituality <a342442746a@126.com>
2021-04-23 17:24:26 +0800

    createTrackbar

--- Retinex图像增强/源.cpp
+++ Retinex图像增强/源.cpp
@@ -4,13 +4,16 @@
 using namespace std;
 using namespace cv;
 
-void SingleScaleRetinex(const Mat& src, Mat& dst, int sigma);
+void SingleScaleRetinex(int, void*);
+
+Mat src, src_small, src_gray, dst;
+int Gsize = 2;
+int Gsigma = 5;
 
 int main(int argc, char** argv)
 {
-	Mat src, src_small, src_gray, dst;
-	src = imread("D:\\CodeTest IMG\\原图/Image8.tif");
-	//src = imread("D:\\CodeTest IMG\\数字图像处理经典照片/X光胸部透视.png");
+	src = imread("D:\\CodeTest IMG\\原图/Image4.tif");
+	//src = imread("D:\\CodeTest IMG\\数字图像处理经典照片/LENA.bmp");
 	if (src.empty())
 	{
 		cout << "could not load image..." << endl;
@@ -28,29 +31,40 @@ int main(int argc, char** argv)
 	namedWindow("gray", CV_WINDOW_AUTOSIZE);
 	imshow("gray", src_gray);
 
-	SingleScaleRetinex(src_gray, dst, 0.5);
-
+	//namedWindow("高斯模糊", CV_WINDOW_AUTOSIZE);
 	namedWindow("output", CV_WINDOW_AUTOSIZE);
-	imshow("output", dst);
+	createTrackbar("Gsize", "output", &Gsize, 15, SingleScaleRetinex);
+	createTrackbar("Gsigma", "output", &Gsigma, 15, SingleScaleRetinex);
+	SingleScaleRetinex(0, 0);
+
+	
+	//imshow("output", dst);
 
 	waitKey(0);
 
 	return 0;
 }
 
-void SingleScaleRetinex(const Mat& src, Mat& dst, int sigma)
+void SingleScaleRetinex(int, void*)
 {
 	Mat doubleImage, gaussianImage, logIImage, logGImage, logRImage;
 	//转换范围，所有图像元素增加1.0保证log操作正常,防止溢出
-	src.convertTo(doubleImage, CV_64FC3, 1.0, 1.0);
+	src_gray.convertTo(doubleImage, CV_64FC3, 1.0, 1.0);
+
 	//高斯模糊，当size为零时将通过sigma自动进行计算
-	GaussianBlur(doubleImage, gaussianImage, Size(3, 3), sigma);
+	GaussianBlur(doubleImage, gaussianImage, Size(Gsize * 2 + 1, Gsize * 2 + 1), Gsigma * 2 + 1);
 	//OpenCV的log函数可以计算出对数值。logIImage和logGImage就是对数计算的结果。
 	log(doubleImage, logIImage);
 	log(gaussianImage, logGImage);
 	//Retinex公式，Log(R(x,y))=Log(I(x,y))-Log(G(x,y)))
 	logRImage = logIImage - logGImage;
 	//将结果量化到[0,255]范围内，NORM_MINMAX表示线性量化，CV_8UC3表示将图像转回
-	normalize(logRImage, dst, 0, 255, NORM_MINMAX, CV_8UC3);
-
+	//exp(logRImage, logRImage);
+	//normalize(logIImage, logIImage, 0, 255, NORM_MINMAX, CV_8UC1);
+	//normalize(logGImage, logGImage, 0, 255, NORM_MINMAX, CV_8UC1);
+	//imshow("原图对数", logIImage);
+	//imshow("高斯模糊", logGImage);
+	//imshow("反射图像", logRImage);
+	normalize(logRImage, dst, 0, 255, NORM_MINMAX, CV_8UC1);
+	imshow("output", dst);
 }
